66eaa5d525f5841bfcd4d96469e09be7eff4d403
stronggirlSY <leesyeon0310@gmail.com>
2024-07-20 21:39:44 +0900

    textinput 하긴 했는데 아직 텍스트를 눌러서 변화시키진 못함, 수정 버튼 그대로임..

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React, {useState} from "react";
-import {StyleSheet, Text, View, Button } from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('이서연');
   const [age, setAge] = useState('20');
   const [email, setEmail] = useState('leesyeon0310@gmail.com');
-  const clickHandler = () => {
-    setName('서연');
-  }
+
   return (
     <View style={styles.container}>
-      <Text>이름: {name}, 나이: {age}, 이메일: {email} </Text>
+      <Text>이름:</Text>
+      <TextInput style={styles.input}/>
       <View style={styles.buttonContainer}>
-        <Button title='수정' onPress={clickHandler}/>
+        <Button title='수정'></Button>
       </View>
     </View>
   );
@@ -29,5 +28,12 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginTop: 20
+  },
+  input: {
+    borderWidth:1,
+    borderColor:'#777',
+    padding: 8,
+    margin: 10,
+    width: 300,
   }
 });
