1f34ab4cc833f84adc4ad962f6e12a0b2faa5d97
stronggirlSY <leesyeon0310@gmail.com>
2024-07-21 00:33:04 +0900

    누르면 textinput 까지 뜬다!!!!!!!11

--- App.js
+++ App.js
@@ -7,25 +7,39 @@ const App =() => {
   const [name, setName] = useState('이서연');
   const [age, setAge] = useState('20');
   const [email, setEmail] = useState('leesyeon0310@gmail.com');
+  const [isEditing, setIsEditing] = useState(false);
 
   const handlePress = () => {
     setPressed(!pressed);
   };
+  const handleEditToggle = () => {
+    setIsEditing(!isEditing);
+  };
 
+  const handleButtonPress = () => {
+    handlePress();
+    handleEditToggle(); 
+  };
   return (
+    
     <View style={styles.container}>
       <Text>이름:</Text>
+      {isEditing ? (
       <TextInput 
       style={styles.input}
       placeholder="이름을 입력하세요"
       value={name}
       onChangeText={setName}
+      onSubmitEditing={handleEditToggle}
       />
+      ) : (
+        <Text style={styles.text}>{name}</Text>
+      )} 
       <View style={styles.buttonContainer}>
         <Button
         color="#2c2c2c"
         title={pressed ? '수정' : '저장'}
-        onPress={handlePress}
+        onPress={handleButtonPress}
          />
       </View>
     </View>
