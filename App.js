2844ca858917e589378ed1f9b89e7563812b38f5
stronggirlSY <leesyeon0310@gmail.com>
2024-07-21 20:15:32 +0900

    1단계와ㄴ성!!!!!!!!!

--- App.js
+++ App.js
@@ -4,11 +4,14 @@ import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 const App =() => {
  
   const [Namepressed, setNamePressed] = useState(false);
+  const [Agepressed,setAgePressed] = useState(false);
+  const [Emailpressed,setEmailPressed] = useState(false);
   const [name, setName] = useState('이서연');
   const [age, setAge] = useState('20');
   const [email, setEmail] = useState('leesyeon0310@gmail.com');
   const [isnameEditing, setIsNameEditing] = useState(false);
   const [isageEditing, setIsageEditing] = useState(false);
+  const [isEmailEditing,setIsEmailEditing] = useState(false);
 
 
   const handleNamePress = () => {
@@ -22,6 +25,35 @@ const App =() => {
     handleNamePress();
     handleNameEditToggle(); 
   };
+// 이름정의
+
+  const handleAgePress = () => {
+    setAgePressed(!Agepressed);
+  };
+
+  const handleAgeEditToggle = () => {
+    setIsageEditing(!isageEditing);
+  };
+
+  const handleAgeButtonPress = () => {
+    handleAgePress();
+    handleAgeEditToggle();
+  };
+
+// 나이정의
+
+const handleEmailPress = () => {
+  setEmailPressed(!Emailpressed);
+};
+
+const handleEmailEditToggle = () => {
+  setIsEmailEditing(!isEmailEditing);
+};
+
+const handleEmailButtonPress = () => {
+  handleEmailPress();
+  handleEmailEditToggle();
+};
 
   return (
 
@@ -29,8 +61,8 @@ const App =() => {
     <View style={styles.container}>
 
       <Text>이름:</Text>
-      {isnameEditing ? (
-      <TextInput 
+      {isnameEditing ?
+       (<TextInput 
       style={styles.input}
       placeholder="이름을 입력하세요"
       value={name}
@@ -42,9 +74,49 @@ const App =() => {
 
       <View style={styles.buttonContainer}>
         <Button
-        color= "#000000"
+        color= "#ffffff"
         title={Namepressed ? '저장' : '수정'}
         onPress={handleNameButtonPress}
+         />
+      </View>
+
+      <Text>나이:</Text>
+      {isageEditing ?
+       (<TextInput 
+      style={styles.input}
+      placeholder="나이를 입력하세요"
+      value={age}
+      onChangeText={setAge}
+      onSubmitEditing={handleAgeEditToggle}/>
+      ) : (
+        <Text style={styles.text}>{age}</Text>
+      )} 
+
+      <View style={styles.buttonContainer}>
+        <Button
+        color= "#ffffff"
+        title={Agepressed ? '저장' : '수정'}
+        onPress={handleAgeButtonPress}
+         />
+      </View>
+
+      <Text>이메일:</Text>
+      {isEmailEditing ?
+       (<TextInput 
+      style={styles.input}
+      placeholder="이메일을 입력하세요"
+      value={email}
+      onChangeText={setEmail}
+      onSubmitEditing={handleEmailEditToggle}/>
+      ) : (
+        <Text style={styles.text}>{email}</Text>
+      )} 
+
+      <View style={styles.buttonContainer}>
+        <Button
+        color= "#ffffff"
+        title={Emailpressed ? '저장' : '수정'}
+        onPress={handleEmailButtonPress}
          />
       </View>
     </View>
@@ -61,7 +133,11 @@ const styles = StyleSheet.create({
     fontSize:30,
   },
   buttonContainer: {
-    marginTop: 20
+    marginTop: 15,
+    merginBottum: 10,
+    backgroundColor: "#0000FF",
+    paddingHorizontal: 5,
+    borderRadius:5,
   },
   input: {
     borderWidth:1,
