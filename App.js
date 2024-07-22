22ca0d06a5b567cfee2f9aa70674c111c5ae2ef1
stronggirlSY <leesyeon0310@gmail.com>
2024-07-23 03:13:20 +0900

    완성못함끝없는실패............

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const App =() => {
  
@@ -13,17 +14,18 @@ const App =() => {
   const [isageEditing, setIsageEditing] = useState(false);
   const [isEmailEditing,setIsEmailEditing] = useState(false);
 
+ 
 
   const handleNamePress = () => {
     setNamePressed(!Namepressed);
   };
   const handleNameEditToggle = () => {
-    setIsNameEditing(!isnameEditing);
+    setIsNameEditing(!isnameEditing); //핸들네임에딧토글: 이즈내임에디팅(기본값이false(수정))를 true(완료)로 반전시키는 역할을 함 
   };
 
   const handleNameButtonPress = () => {
     handleNamePress();
-    handleNameEditToggle(); 
+    handleNameEditToggle();  //핸들네임버튼프레스->핸들네임프레스,핸들네임에딧토글 두개가 동시에 시현되도록 함
   };
 // 이름정의
 
@@ -54,29 +56,51 @@ const handleEmailButtonPress = () => {
   handleEmailPress();
   handleEmailEditToggle();
 };
+// 이메일 정의
+
+
+
+
+ //취미
+
+ const [hobby, setHobby] = useState(false);
+ const [hobbypressed, setHobbypressed] = useState(false); 
+
+ const hobbypress = () => {
+  setHobbypressed(!hobbypressed);
+ }; //하비프레스: 하비프레스드(false)를 true인 상황으로 바꿔줌
+
+ submitBtn = () => {
+  this.setState({text: this.state.inputText});
+}
+
+state = {
+  text: '',
+  inputText:''
+}
 
   return (
 
     //이름칸
+   
     <KeyboardAwareScrollView style={styles.container}>
-      <Text>이름:</Text>
-      {isnameEditing ?
-       (<TextInput 
-      style={styles.input}
-      placeholder="이름을 입력하세요"
-      value={name}
-      onChangeText={setName}
-      onSubmitEditing={handleNameEditToggle}/>
-      ) : (
-        <Text style={styles.text}>{name}</Text>
-      )} 
-
+    <Text>이름:</Text>
+    {isnameEditing ? // true -> 인풋필드 false ->텍스트 렌더링
+     (<TextInput    // 인풋 필드창이 뜬다
+    style={styles.input}
+    placeholder="이름을 입력하세요" 
+    value={name} // 입력 필드의 현재 값 설정
+    onChangeText={setName} // 인풋 필드창에 텍스트를 입력할때마다 입력하는 값을 name으로 만듦 (새로고침처럼)
+    onSubmitEditing={handleNameEditToggle}/> // 종료하면 핸들네임에딧토글로 편집모드 종료 
+    ) : (
+      <Text style={styles.text}>{name}</Text>
+    )} 
       <View style={styles.buttonContainer}>
         <Button
         color= "#ffffff"
         title={Namepressed ? '저장' : '수정'}
         onPress={handleNameButtonPress}
-         />
+        />
       </View>
 
       <Text>나이:</Text>
@@ -118,22 +142,44 @@ const handleEmailButtonPress = () => {
         onPress={handleEmailButtonPress}
          />
       </View>
-    </KeyboardAwareScrollView>>
-  );
+
+      // 일단 이까지가 버튼 칸 해결해야할것: 엑스포 찍었을때 오류나는거;(이건뷰를스크롤뷰로바꿔서.)
+
+      //여기서부터 취미...
+
+      <Text>취미 목록</Text>
+      <TextInput
+      style={styles.input}
+      value={hobby}
+      onChangeText={(text) => this.setState.({inputText: text})}
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button title="입력" 
+        onPress={this.submitBtn}
+        />
+      </View>
+      
+
+
+
+
+    </KeyboardAwareScrollView>
+   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    justifyContent: 'left',
   },
   text: {
     fontSize:30,
   },
   buttonContainer: {
     marginTop: 15,
-    merginBottum: 10,
+    merginBottom: 10,
     backgroundColor: "#0000FF",
     paddingHorizontal: 5,
     borderRadius:5,
