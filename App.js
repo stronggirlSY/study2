e7e684e303ddcf8829d53985cb67f92133f1f7a4
stronggirlSY <leesyeon0310@gmail.com>
2024-07-25 22:00:32 +0900

    ㅠㅠ

--- App.js
+++ App.js
@@ -20,22 +20,6 @@ const App =() => {
 
  //취미
  
-//  const [hobby, setHobby] = useState(false);
-//  const [hobbypressed, setHobbypressed] = useState(false); 
-
-//  const hobbypress = () => {
-//   setHobbypressed(!hobbypressed);
-//  }; //하비프레스: 하비프레스드(false)를 true인 상황으로 바꿔줌
-
-//  submitBtn = () => {
-//   this.setState({text: this.state.inputText});
-// }
-
-// state = {
-//   text: '',
-//   inputText:''
-// }
-
   return (
 
     //이름칸
@@ -111,25 +95,7 @@ const App =() => {
           </View>
         </View>
 
-        {/* //일단 이까지가 버튼 칸 해결해야할것: 엑스포 찍었을때 오류나는거;(이건뷰를스크롤뷰로바꿔서.이건다시뷰로바꿔놓긴함..)
-
-        //여기서부터 취미... 만들어야할것:입력필드 안에 쓴 게 밖으로 text로 구현되도록
-
         <Text>취미 목록</Text>
-        <TextInput
-        style={styles.input}
-        value={hobby}
-        onChangeText={(text) => this.setState({inputText: text})}
-        />
-
-        <View style={styles.buttonContainer}>
-          <Button title="입력" 
-          onPress={this.submitBtn}
-          />
-        </View>
-
-
-        //list 형태로 정의하기  */}
       </View>
     </KeyboardAwareScrollView>
    );
